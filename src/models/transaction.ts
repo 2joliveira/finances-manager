f59dd002dd2fabecd6fa37b604f8251c614c815f
Jefferson Oliveira <oliveira2112@outlook.com>
2026-03-02 14:49:36 -0300

    config form with react hook form

---
+++ src/models/transaction.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const transactionSchema = z.object({
+  description: z
+    .string({ error: "Descrição é obrigatória." })
+    .min(4, { error: "Descrição deve ter mais de 4 letras." }),
+  amount: z.number({ error: "Valor é obrigatório." }),
+  type: z.enum(["income", "expense"], {
+    error: "Tipo deve ser 'income' ou 'expense'.",
+  }),
+  is_installment: z.boolean().default(false),
+  installments: z.number().optional(),
+  category_id: z.number({ error: "Categoria é obrigatória." }),
+  account_id: z.number({ error: "Conta é obrigatória." }),
+  transaction_date: z.string({ error: "Data da transação é obrigatória." }),
+});
+
+export type Transaction = z.infer<typeof transactionSchema>;
--- src/screens/Home/components/TransactionFormModal/index.tsx
+++ src/screens/Home/components/TransactionFormModal/index.tsx
@@ -5,6 +5,9 @@ import { colors } from "@/theme";
 import { styles } from "./styles";
 import { InputSwitch } from "../../../../components/InputSwitch";
 import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Transaction, transactionSchema } from "@/models/transaction";
 
 interface TransactionFormModalProps {
   isOpen: boolean;
@@ -15,8 +18,29 @@ export function TransactionFormModal({
   isOpen,
   setIsOpen,
 }: TransactionFormModalProps) {
-  const [isInstallment, setIsInstallment] = useState("Não");
-  const [transactionType, setTransactionType] = useState("Receita");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      description: "",
+      amount: 0,
+      type: "expense",
+      is_installment: false,
+      installments: 2,
+      category_id: 0,
+      account_id: 0,
+      transaction_date: new Date().toISOString(),
+    },
+  });
+
+  function onSubmit(data: Transaction) {
+    console.log(data);
+  }
+
+  console.log({ errors });
 
   return (
     <Modal isVisible={isOpen}>
@@ -36,57 +60,99 @@ export function TransactionFormModal({
         <View>
           <Text style={styles.label}>Descrição</Text>
 
-          <TextInput
-            style={styles.input}
-            placeholder="Ex: Almoço no restaurante"
-            placeholderTextColor={colors.gray[400]}
+          <Controller
+            control={control}
+            name="description"
+            render={({ field: { value, onChange } }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Ex: Almoço no restaurante"
+                placeholderTextColor={colors.gray[400]}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
           />
         </View>
 
         <View>
           <Text style={styles.label}>Valor</Text>
 
-          <TextInput
-            style={styles.input}
-            placeholder="Ex: R$ 50,00"
-            placeholderTextColor={colors.gray[400]}
+          <Controller
+            control={control}
+            name="amount"
+            render={({ field: { value, onChange } }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Ex: R$ 50,00"
+                placeholderTextColor={colors.gray[400]}
+                value={String(value)}
+                onChangeText={onChange}
+                keyboardType="decimal-pad"
+              />
+            )}
           />
         </View>
 
         <View>
           <Text style={styles.label}>Tipo da transação</Text>
 
-          <InputSwitch
-            options={["Receita", "Despesa"]}
-            value={transactionType}
-            onChange={setTransactionType}
+          <Controller
+            control={control}
+            name="type"
+            render={({ field: { value, onChange } }) => (
+              <InputSwitch
+                options={["Receita", "Despesa"]}
+                value={value}
+                onChange={onChange}
+              />
+            )}
           />
         </View>
 
-        <View style={{ flexDirection: "row", justifyContent: "space-between", gap: 10 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            gap: 10,
+          }}
+        >
           <View>
             <Text style={styles.label}>Compra parcelada</Text>
 
-            <InputSwitch
-              options={["Sim", "Não"]}
-              value={isInstallment}
-              onChange={setIsInstallment}
+            <Controller
+              control={control}
+              name="is_installment"
+              render={({ field: { value, onChange } }) => (
+                <InputSwitch
+                  options={["Sim", "Não"]}
+                  value={true ? "Sim" : "Não"}
+                  onChange={onChange}
+                />
+              )}
             />
           </View>
 
           <View>
             <Text style={styles.label}>Número de parcelas</Text>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Ex: 2"
-              placeholderTextColor={colors.gray[400]}
-              keyboardType="decimal-pad"
+            <Controller
+              control={control}
+              name="installments"
+              render={({ field: { value, onChange } }) => (
+                <TextInput
+                  style={styles.input}
+                  placeholder="Ex: 2"
+                  placeholderTextColor={colors.gray[400]}
+                  value={String(value)}
+                  onChangeText={onChange}
+                />
+              )}
             />
           </View>
         </View>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleSubmit(onSubmit)}>
           <View style={styles.button}>
             <Text style={styles.buttonText}>Criar Transação</Text>
           </View>
